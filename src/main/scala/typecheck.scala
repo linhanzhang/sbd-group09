8c4c25cc6069e3c1d580768f32bd8fa23db65019
Kevin_Liu <40707961+Kevin6Liu@users.noreply.github.com>
2021-11-08 23:51:30 +0100

    Update typecheck.scala

--- src/main/scala/typecheck.scala
+++ src/main/scala/typecheck.scala
@@ -8,7 +8,11 @@ object typecheck {
   def matchFunc(s: String): Int = {
     toInt(s) match {
       case Success(i) =>
-        if (!(s.toInt > 0) || !(s.toInt < 96)) {
+        // The highest elevation of a city in the Netherlands is 96 meters.
+        // In this case, if the sea level is above 96 meters, there will be no
+        // safe place to go.
+        // So a valid input should between 0 ~ 8848
+        if (!(s.toInt >= 0) || !(s.toInt < 8849)) {
           println("******************************************************")
           println(" Please enter an integer between 0 ~ 96(not included) ")
           return -1
